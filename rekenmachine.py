448eaf3728a07deb097d46311b7e391855d669ef
Anouk2005 <515046@vistacollege.nl>
2022-11-18 09:39:05 +0100

    tht4y5y35555

---
+++ rekenmachine.py
@@ -0,0 +1,15 @@
+getal_1= float(input("eerste getal?"))
+
+operator= input("Operator?")
+
+getal_2= float(input("tweede getal?"))
+
+if operator == '+' :
+    print ("resultaat:", getal_1+ getal_2)
+elif operator == '-' :
+    print ("resultaat:", getal_1- getal_2)
+elif operator==  '*' :
+ print ("resultaat:",getal_1* getal_2)
+else:
+    print ("resultaat:", getal_1/ getal_2)
+
